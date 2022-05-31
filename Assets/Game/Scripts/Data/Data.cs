840ca4fc1bb7d4cbfce93c0f61779ad470653aa6
Konev_Daniil <konev.danKo@yandex.ru>
2022-05-31 23:04:27 +0300

    Analytic

---
+++ Assets/Game/Scripts/Data/Data.cs
@@ -0,0 +1,103 @@
+using System;
+using UnityEngine;
+
+public class Data : MonoBehaviour
+{
+    protected const string _dataKeyName = "AutoAccident";
+    protected SaveOptions _options = new SaveOptions();
+
+    public void Save()
+    {
+        string json = JsonUtility.ToJson(_options);
+        PlayerPrefs.SetString(_dataKeyName, json);
+    }
+
+    public void Load()
+    {
+        if (PlayerPrefs.HasKey(_dataKeyName) == false)
+        {
+            _options = new SaveOptions();
+            Save();
+        }
+        else
+            _options = JsonUtility.FromJson<SaveOptions>(PlayerPrefs.GetString(_dataKeyName));
+    }
+
+    public void RemoveData()
+    {
+        PlayerPrefs.DeleteKey(_dataKeyName);
+        _options = new SaveOptions();
+    }
+
+    public void SetLevelIndex(int index)
+    {
+        _options.LevelNumber = index;
+    }
+
+    public void SetDateRegistration(DateTime date)
+    {
+        _options.RegistrationDate = date.ToString();
+    }
+
+    public void SetLastLoginDate(DateTime date)
+    {
+        _options.LastLoginDate = date.ToString();
+    }
+
+    public void SetGamePoints(int value)
+    {
+        _options.GamePointsCount = value;
+    }
+
+    public void AddSession()
+    {
+        _options.SessionCount++;
+    }
+
+    public int GetLevelIndex()
+    {
+        return _options.LevelNumber;
+    }
+
+    public int GetSessionCount()
+    {
+        return _options.SessionCount;
+    }
+
+    public void AddDisplayedLevelNumber()
+    {
+        _options.DisplayedLevelNumber++;
+    }
+
+    public int GetNumberDaysAfterRegistration()
+    {
+        return (DateTime.Parse(_options.LastLoginDate) - DateTime.Parse(_options.RegistrationDate)).Days;
+    }
+
+    public int GetDisplayedLevelNumber()
+    {
+        return _options.DisplayedLevelNumber;
+    }
+
+    public string GetRegistrationDate()
+    {
+        return _options.RegistrationDate;
+    }
+
+    public int GetGamePointsCount()
+    {
+        return _options.GamePointsCount;
+    }
+}
+
+[Serializable]
+public class SaveOptions
+{
+    public int LevelNumber;
+    public int SessionCount;
+    public string LastLoginDate;
+    public string RegistrationDate;
+    public int DisplayedLevelNumber = 1;
+    public int GamePointsCount;
+}
+
---
+++ Assets/Game/Scripts/Data/GameSceneManager.cs
@@ -0,0 +1,65 @@
+using UnityEngine;
+using TMPro;
+using UnityEngine.SceneManagement;
+
+public class GameSceneManager : Data
+{
+/*    [SerializeField] private TMP_Text _levelText;*/
+    [SerializeField] private AppMetricaEvents _appMetricaObject;
+    [SerializeField] private GameAnalyticsObject _gameAnalyticsObject;
+
+    private int _nextLevelIndex;
+
+    private void Awake()
+    {
+        Load();
+        SetLevelIndex(SceneManager.GetActiveScene().buildIndex);
+        Save();
+/*        _levelText.text = $"Level {GetDisplayedLevelNumber()}";*/
+    }
+
+    private void Start()
+    {
+        _appMetricaObject.OnLevelStart(GetDisplayedLevelNumber());
+        _gameAnalyticsObject.OnLevelStart(GetDisplayedLevelNumber());
+    }
+
+    private void OnApplicationQuit()
+    {
+        _appMetricaObject.OnGameExit(GetRegistrationDate(), GetSessionCount(), GetNumberDaysAfterRegistration());
+        _gameAnalyticsObject.OnGameExit(GetRegistrationDate(), GetSessionCount(), GetNumberDaysAfterRegistration());
+        Save();
+    }
+
+    private void SaveNextLevelIndex()
+    {
+        SetLevelIndex(_nextLevelIndex);
+        AddDisplayedLevelNumber();
+    }
+
+    public void LevelFail()
+    {
+        _appMetricaObject.OnFail(GetDisplayedLevelNumber());
+        _gameAnalyticsObject.OnFail(GetDisplayedLevelNumber());
+    }
+
+    public void LoadNextScene()
+    {
+        _appMetricaObject.OnLevelComplete(GetDisplayedLevelNumber());
+        _gameAnalyticsObject.OnLevelComplete(GetDisplayedLevelNumber());
+        Save();
+        if (SceneManager.GetActiveScene().buildIndex + 1 >= SceneManager.sceneCountInBuildSettings)
+            _nextLevelIndex = 1;
+        else
+            _nextLevelIndex = SceneManager.GetActiveScene().buildIndex + 1;
+        SaveNextLevelIndex();
+        SceneManager.LoadScene(_nextLevelIndex);
+    }
+
+    public void ReloadScene()
+    {
+        _appMetricaObject.OnLevelRestart(GetDisplayedLevelNumber());
+        _gameAnalyticsObject.OnLevelRestart(GetDisplayedLevelNumber());
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+    }
+}
